17a50a1be2f5180c7bcfb4500ece6f08afb553f2
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-20 22:11:35 +0300

    Create Movie class Created a movie class where i instanciated and test for instanciation

---
+++ app/models/movie.py
@@ -0,0 +1,12 @@
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,image,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.image =image
+        self.vote_average = vote_average
+        self.vote_count = vote_count
---
+++ app/movie_test.py
@@ -0,0 +1,21 @@
+import unittest
+from models import movie
+Movie = movie.Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
+
+if __name__ == '__main__':
+    unittest.main()
