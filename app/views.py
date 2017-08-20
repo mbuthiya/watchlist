25940f2f69dc357596450e834f7d5b1c82b03f9e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-20 19:52:03 +0300

    Add dynamic route
    Added a movie route with a dynamic entry that takes the movie id. and displays
     data

--- app/views.py
+++ app/views.py
@@ -6,5 +6,20 @@ from app import app
 # Views
 @app.route('/')
 def index():
+    
+    '''
+    View root page function that returns the index page and its data
+    '''
+
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+
+    return render_template('movie.html')
