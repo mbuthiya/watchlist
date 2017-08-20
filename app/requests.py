ed26288460c82fb67232d8b6287feec8c1945d9b
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-20 22:33:15 +0300

    Create first api request class

--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,24 @@
 from app import app
+import urllib.request,json
+
 
 # Getting api key
 api_key = app.config['MOVIE_API_KEY']
 
+# Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
+
+
 def get_movies(category):
-    pass
+    get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results_list = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+
+    return movie_results_list
