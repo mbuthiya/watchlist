6499f0fe0419e4125cde94c7a21148254e7c2be2
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-20 19:57:46 +0300

    Add title variables Created title variables and passed them to the response statement of the view funcions

--- app/views.py
+++ app/views.py
@@ -6,13 +6,13 @@ from app import app
 # Views
 @app.route('/')
 def index():
-    
+
     '''
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
 
 @app.route('/movie/<movie_id>')
@@ -21,5 +21,5 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-
-    return render_template('movie.html')
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title)
