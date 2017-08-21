50940215438067713930e724ea61feef69a9043a
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 17:18:08 +0300

    Error handler update

--- app/error.py
+++ app/error.py
@@ -6,4 +6,4 @@ def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourOwfour.html')400
+    return render_template('fourOwfour.html'),400
