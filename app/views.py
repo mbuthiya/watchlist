b6d55e4e310036d5ee51337166ed0437e79930b8
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 07:42:36 +0300

    Create for loops to display list of objects

--- app/views.py
+++ app/views.py
@@ -14,9 +14,9 @@ def index():
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_movies)
 
 
 @app.route('/movie/<movie_id>')
