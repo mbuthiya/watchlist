109d362910054458e741b8016688f4f669185540
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 11:54:47 +0300

    Update Macro to get and display the images

--- app/models/movie.py
+++ app/models/movie.py
@@ -3,10 +3,10 @@ class Movie:
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,id,title,overview,image,vote_average,vote_count):
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
         self.id =id
         self.title = title
         self.overview = overview
-        self.image = "https://image.tmdb.org/t/p/w500/" + image
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
         self.vote_average = vote_average
         self.vote_count = vote_count
