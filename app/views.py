7a19fd04b5f5735d114a287243627c4ed11f0e31
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 15:28:23 +0300

    UPdate movie page title

--- app/views.py
+++ app/views.py
@@ -27,6 +27,6 @@ def movie(id):
     View movie page function that returns the movie details page and its data
     '''
     movie = get_movie(id)
-    title = f'You are viewing {movie.title}'
+    title = f'{movie.title}'
 
     return render_template('movie.html',title = title,movie = movie)
