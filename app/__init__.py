58b79ae3211d1e9d519c859887fbe7e7ec774210
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 10:51:43 +0300

    Initialize bootstrap Initialized bootstrap inside the app init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -7,6 +8,9 @@ app = Flask(__name__,instance_relative_config = True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 
 from app import views
