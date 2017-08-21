fea4f8640ab49d8346783bccabd88c96d2d60027
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 17:30:13 +0300

    Add 404 error page

--- app/error.py
+++ app/error.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from app import app
 
-@app.errorhandler(400)
+@app.errorhandler(404)
 def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourOwfour.html'),400
+    return render_template('fourOwfour.html'),404
