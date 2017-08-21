b2d706090420be2a9cb5152443232c0a4ba9d367
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 09:40:44 +0300

    Update init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from .config import DevConfig,ProdConfig
+from .config import DevConfig
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
-app.config.from_object(ProdConfig)
+app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
 
 
