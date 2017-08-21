1bbb397e661e8f20624ed4d77ab7b2ec36aa87e0
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-21 07:51:00 +0300

    Created api requests for other items

--- app/views.py
+++ app/views.py
@@ -14,9 +14,10 @@ def index():
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
 @app.route('/movie/<movie_id>')
