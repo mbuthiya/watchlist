86133da25414128a20acc79f4039850ec6146710
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-22 12:07:31 +0300

    Created movie review class

---
+++ app/review_test.py
@@ -0,0 +1,30 @@
+import unittest
+from models import review
+Review = review.Review
+
+class TestReview(unittest.TestCase):
+
+    def setUp(self):
+        self.new_review = Review(12345,'Review for movies',"https://image.tmdb.org/t/p/w500/jdjdjdjn",'This movie is the best thing since sliced bread')
+
+
+    def tearDown(self):
+        Review.clear_reviews()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_review,Review))
+
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_review.movie_id,12345)
+        self.assertEquals(self.new_review.title,'Review for movies')
+        self.assertEquals(self.new_review.imageurl,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEquals(self.new_review.review,'This movie is the best thing since sliced bread')
+
+
+    def test_save_review(self):
+        self.new_review.save_review()
+        self.assertTrue(len(Review.all_reviews)>0)
+
+if __name__ == '__main__':
+    unittest.main()
