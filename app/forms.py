e1ed4eb5493d858c645e721157e8961278a1dc2d
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-22 15:01:44 +0300

    Created Form logic to create new review object

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(Form):
+class ReviewForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie revie', validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
     submit = SubmitField('Submit')
--- app/views.py
+++ app/views.py
@@ -2,6 +2,9 @@ from flask import render_template,request,redirect,url_for
 from app import app
 from .requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
+from .models import review
+
+Review = review.Review
 
 
 
@@ -55,7 +58,17 @@ def search(movie_name):
 def new_review(id):
 
     form = ReviewForm()
-    
+
     movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+
+        return redirect(url_for('movie',id = movie.id ))
+
     title = f'{movie.title} review'
-    return render_template('new_review.html',title = title)
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
