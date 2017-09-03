3c89c3dbb3ab3415e1307a61042e3f6684852c24
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-04 02:28:00 +0300

    CReated a role model to define the different roles of the user

--- app/models.py
+++ app/models.py
@@ -45,12 +45,26 @@ class Review:
         return response
 
 
+
 class User(db.Model):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy=dynamic)
+
+
+    def __repr__(self):
+        return f'User {self.name}'
