a2b95c7060b4f4450892dcf299be4039c6b30d22
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-04 01:55:11 +0300

    Created a shell context inside manager file

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
-from flask_script import Manager,Shell,Server
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
@@ -14,6 +15,11 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(testsg)
+
+@manager.shell
+def make_shell_context():
+
+    return dict(app = app,db = db,User = User )
 
 if __name__ == '__main__':
     manager.run()
