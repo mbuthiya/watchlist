f79976a1efa8026775a855bfc9154aaae81e41b7
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-03 08:18:38 +0300

    Created configuration for database uri

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
 
     @staticmethod
     def init_app(app):
