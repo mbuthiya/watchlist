007dcb6c4684956c3c419b85d27a633ecaac1d97
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-05 16:25:10 +0300

    Created registration form class

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,3 +24,7 @@ def logout():
     logout_user()
     flash('You have been successfully logged out')
     return redirect(url_for("main.index"))
+
+@auth.route('/register')
+def register():
+    pass
