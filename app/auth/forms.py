37daf520243e3a0fe00fdc0f7869f55460fbf15f
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-05 14:17:55 +0300

    Created a sign in form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Length(1,64),Email()])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,19 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
+from flask_login import login_user
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+        title = "watchlist login"
+
+    return render_template('auth/login.html',login_form = login_form,title=title)
