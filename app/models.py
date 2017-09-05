c85587c5e80ecd2775659d6c221b140961c2fc37
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-05 11:29:31 +0300

    Added the load user callback

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Movie:
     '''
