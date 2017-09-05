2c4e43948d92bdc965f0c432a9cb49983043a5ca
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-05 09:10:35 +0300

    Fixed tsting errors

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(testsg)
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
