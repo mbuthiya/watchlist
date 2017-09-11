026057471fa90a9a51be6de9b42d662870a5df47
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-12 01:56:40 +0300

    Created config files for the flask mail extension

--- config.py
+++ config.py
@@ -9,6 +9,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     @staticmethod
     def init_app(app):
         pass
