d95a331812af45afb84d1f20a6f127df0a3d9838
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-11 16:06:09 +0300

    Created config path for photo upload

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     @staticmethod
     def init_app(app):
