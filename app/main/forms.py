03d27b8eda42ec70df7dfc9835ee33f107565e4e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-11 14:35:38 +0300

    Create update form class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email
 
 class ReviewForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
     review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    username = StringField('Username',validators = [Required()])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    bio = StringField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
