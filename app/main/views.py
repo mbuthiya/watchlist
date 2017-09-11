672cbf83e2b04bd363cf4d099105199ae4e170f5
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-11 16:16:39 +0300

    Uploaded an image

--- app/main/views.py
+++ app/main/views.py
@@ -112,14 +112,14 @@ def update_profile(uname):
     return render_template('profile/update.html',form =form)
 
 
-@main.route('/user/<uname>/updatepic',methods= ['POST'])
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
+        print(path)
         user.profile_pic_path = path
         db.session.commit()
-
-        return redirect(url_for('main.profile',uname=uname))
+    return redirect(url_for('main.profile',uname=uname))
