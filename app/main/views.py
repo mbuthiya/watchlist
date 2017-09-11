b900f91e7bbeea22d408d40b9ebec88084c86486
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-11 16:07:47 +0300

    COnfigured file save path

--- app/main/views.py
+++ app/main/views.py
@@ -118,7 +118,8 @@ def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        user.profile_pic_path = filename
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
         db.session.commit()
 
         return redirect(url_for('main.profile',uname=uname))
