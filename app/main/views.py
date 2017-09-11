ab085ef8703c6647c396ed2d049f315168c647c0
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-11 14:43:25 +0300

    Create view function for the update form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
-from . import main
+from . import main,db
 from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
+from .forms import ReviewForm,UpdateProfile
 from ..models import Review,User
 from flask_login import login_required
 
@@ -88,3 +88,23 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html')
