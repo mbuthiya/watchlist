49e3763b82001520c3a8ea92091121e4ed8e3675
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-12 11:58:06 +0300

    Saved reviews

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..models import Review,User,PhotoProfile
-from flask_login import login_required
+from flask_login import login_required,current_user
 from .. import db,photos
 
 
@@ -67,8 +67,8 @@ def new_review(id):
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
-
-        new_review = Review(movie_id=movie.id,title=title,image_path=movie.poster,review=review,user = current_user)
+        user = current_user._get_current_object()
+        new_review = Review(movie_id=movie.id,title=title,image_path=movie.poster,review=review,user = user)
         new_review.save_review()
 
         return redirect(url_for('.movie',id = movie.id ))
