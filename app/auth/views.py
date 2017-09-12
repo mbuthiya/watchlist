35c837df0f13d78af51b8e8fdb24b2c18b61aa54
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-12 11:03:24 +0300

    Create a save user method to save a user

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,11 +32,9 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
+        user.save_user()
 
         mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
-        
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
--- app/models.py
+++ app/models.py
@@ -85,6 +85,11 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
 
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'User {self.username}'
 
