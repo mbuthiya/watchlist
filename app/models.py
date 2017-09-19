ff950b04db4b223237f3b53aa3f2d697b3edcf54
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-19 04:00:59 +0300

    Created reviews model and migrations

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -22,35 +23,29 @@ class Movie:
 
 
 
-class Review:
+class Review(db.Model):
 
-    all_reviews = []
+    __tablename__ = 'reviews'
 
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.Time,default=datetime.utcnow())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
 
     def save_review(self):
-        Review.all_reviews.append(self)
+        db.session.add(self)
+        db.session.commit()
 
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
 
     @classmethod
     def get_reviews(cls,id):
 
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
+        reviews = Review.query.filter_by(movie_id=id).all()
+        return reviews
 
 
 class PhotoProfile(db.Model):
@@ -73,6 +68,8 @@ class User(UserMixin,db.Model):
 
     password_hash = db.Column(db.String(255))
     photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+
     @property
     def password(self):
         raise AttributeError('You cannnot read the password attribute')
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role,PhotoProfile
+from app.models import User,Role,PhotoProfile,Review
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
