ee4a9a28281bb701b4d1673608b8f70b5344dd34
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-19 16:23:52 +0300

    Created test for database queries

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -26,12 +25,16 @@ class Config:
 class ProdConfig(Config):
     pass
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist_test'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 
 }
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from app.models import User,Role,PhotoProfile,Review
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
-app = create_app('development')
+app = create_app('test')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
